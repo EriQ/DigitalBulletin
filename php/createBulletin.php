cd19a0655516993ad18d89016309b13920db4a72
Eric Higdon <mrbassman.10@gmail.com>
2015-10-22 16:07:28 -0500

    Save to database

--- php/createBulletin.php
+++ php/createBulletin.php
@@ -5,27 +5,29 @@ $servername = "localhost";
 $username = "erichigd_bullet";
 $password = "eriQ2930";
 $dbname = "erichigd_digitalBulletin";
+$return = "Bulletin Saved";
 $name = $_POST["name"];
 $content = $_POST["content"];
-/*// Create connection
+// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
+	$return="Connection failed: " . $conn->connect_error;
 } 
 // Prepared statement, stage 1: prepare
-if (!($stmt = $mysqli->prepare("INSERT INTO bulletins(name, content) VALUES (?, ?)"))) {
-    echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+if (!($stmt = $conn->prepare("INSERT INTO bulletins(name, content) VALUES (?, ?)"))) {
+    $return = "Prepare failed: (" . $conn->errno . ") " . $conn->error;
 }
 // Prepared statement, stage 2: bind and execute 
 $id = 1;
 if (!$stmt->bind_param("ss", $name, $content)) {
-    echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
+    $return = "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
 }
 
 if (!$stmt->execute()) {
-    echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+    $return = "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
 }
-$conn->close();*/
-echo "Bulletin Saved";
+$conn->close();
+echo $return;
 ?>
