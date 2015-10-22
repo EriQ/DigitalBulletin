fd1ba2bb5d56ddeedcadb756789b99542746f742
Eric Higdon <mrbassman.10@gmail.com>
2015-10-22 16:15:58 -0500

    Only add the html if the fields exist

--- js/pageLoad.js
+++ js/pageLoad.js
@@ -45,22 +45,25 @@ function loadContent(bulletinName) {
 				var that = this;
 				if(this.repeating)
 				{
-					/*$.each(this.repeatdata, function(index) {
-						var template = $('[templatefield="'+that.templateField+'"] template').html();
-						console.log(template);
-						$.each(this, function() {
-							var pattern = new RegExp("{"+this.id+"}", 'g');
-							template = template.replace(pattern, this.data);
-						});
-						if(that.saving)
+					$.each(this.repeatdata, function(index) {
+						if(this.templateField)
 						{
-							var inputPattern = new RegExp("{input}", 'g');
-							template = template.replace(inputPattern, "<input type='text'/>");
+							var template = $('[templatefield="'+that.templateField+'"] template').html();
+							console.log(template);
+							$.each(this, function() {
+								var pattern = new RegExp("{"+this.id+"}", 'g');
+								template = template.replace(pattern, this.data);
+							});
+							if(that.saving)
+							{
+								var inputPattern = new RegExp("{input}", 'g');
+								template = template.replace(inputPattern, "<input type='text'/>");
+							}
+							var emptyPattern = new RegExp("{(.*?)}", 'g');
+							template = template.replace(emptyPattern, "");
+							$('[templatefield="'+that.templateField+'"]').append(template);
 						}
-						var emptyPattern = new RegExp("{(.*?)}", 'g');
-						template = template.replace(emptyPattern, "");
-						$('[templatefield="'+that.templateField+'"]').append(template);
-					});*/
+					});
 				}
 				if(this.saving)
 				{
